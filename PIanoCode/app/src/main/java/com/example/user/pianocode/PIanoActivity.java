1d89db6d35710be05a83ae0c6d655a7a719f10d0
Joseph Miller <fireyshyguy@gmail.com>
2018-02-23 10:53:41 -0800

    Now have a working piano activity.

--- PIanoCode/app/src/main/java/com/example/user/pianocode/PIanoActivity.java
+++ PIanoCode/app/src/main/java/com/example/user/pianocode/PIanoActivity.java
@@ -14,7 +14,25 @@ public class PIanoActivity extends AppCompatActivity {
     }
 
     public void playSound(View view){
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.a1);
+        int sound = R.raw.a3;
+        switch(view.getId()) {
+            case R.id.key1: sound = R.raw.a3;
+                break;
+            case R.id.key2: sound = R.raw.a4;
+                break;
+            case R.id.key3: sound = R.raw.a5;
+                break;
+            case R.id.key4: sound = R.raw.b3;
+                break;
+            case R.id.key5: sound = R.raw.b4;
+                break;
+            case R.id.key6: sound = R.raw.b5;
+                break;
+            case R.id.key7: sound = R.raw.c3;
+                break;
+        }
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, sound);
         mediaPlayer.start(); // no need to call prepare(); create() does that for you
     }
 }
